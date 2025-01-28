de602d2565d3c220169e78fea982f9b70f4a9433
sahana-mangipudi <smangipudi@uchicago.edu>
2025-01-28 04:47:47 +0000

    running ruby programs

---
+++ howdy.rb
@@ -0,0 +1,25 @@
+my_string = "Hello, world!"
+pp my_string
+
+require "./goodbye.rb"
+
+pp "What's your name?"
+their_name = gets.chomp
+puts "Hello, " + their_name + "!"
+
+require "active_support/all"
+
+1.ordinalize
+2.ordinalize
+53.ordinalize
+2009.ordinalize
+-21.ordinalize
+-134.ordinalize
+
+"table".pluralize
+"ruby".pluralize
+"equipment".pluralize
+
+"tables".singularize
+"rubies".singularize
+"equipment".singularize
